b645cd0849c45904301e4ab8b18fcc0af6e379a8
Bhavesh-Kataria <katariabhavesh6102000@gmail.com>
2025-03-13 12:38:17 +0530

    Added Monthly Wage System

--- src/EmployeeWageHandler.java
+++ src/EmployeeWageHandler.java
@@ -4,29 +4,29 @@ class Employee{
     String name;
     int id;
     boolean isPresentToday;
-    int wageDaily;
-    Employee(String name,int id,boolean isPresentToday,int wageDaily){
+    Employee(String name,int id){
         this.name = name;
         this.id = id;
-        this.isPresentToday = isPresentToday;
-        this.wageDaily = wageDaily;
     }
 
     @Override
     public String toString() {
-        return "Employee [ "+"Name : "+this.name+", Id : "+this.id+", isPresent : "+this.isPresentToday+", Wage : "+this.wageDaily +"]";
+        return "Employee [ "+"Name : "+this.name+", Id : "+this.id+"]";
     }
 }
 
 class EmpOpsHandler{
+    static final int hours = 8;
+    static final int partTimeHours = 4;
+    static final int wagePerHour = 20;
     static boolean empAttendance(){
         int isPresent = (int)Math.round(Math.random());
         return isPresent == 1 ;
     }
+    static int calculateWorkingHours(int logIn, int logOut){
+        return logOut - logIn;
+    }
     static int wageCalculator(int presentHours){
-        final int hours = 8;
-        final int partTimeHours = 4;
-        final int wagePerHour = 20;
         switch (presentHours){
             case 1 :
             case 2 :
@@ -42,23 +42,31 @@ public class EmployeeWageHandler {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int wage = 0;
-        boolean setAttendance = EmpOpsHandler.empAttendance();
-        if(setAttendance){
-            System.out.println("Enter your log in time");
-            int logInTime = sc.nextInt();
-            System.out.println("Enter your log out time");
-            int logOutTime = sc.nextInt();
-            int presentHours = logOutTime - logInTime;
-            wage = EmpOpsHandler.wageCalculator(presentHours);
+        int fullDays = 0;
+        int halfDays = 0;
+        int monthlyWage = 0;
+        int logInTime = 0;
+        int logOutTime = 0;
+        int presentHours = 0;
+        boolean setAttendance;
+        Employee emp = new Employee("Bhavesh", 1);
+        for (int i =1 ; i<=20 ; i++){
+            setAttendance = EmpOpsHandler.empAttendance();
+            if(setAttendance){
+                System.out.println("Enter your log in time");
+                logInTime = sc.nextInt();
+                System.out.println("Enter your log out time");
+                logOutTime = sc.nextInt();
+                presentHours = EmpOpsHandler.calculateWorkingHours(logInTime,logOutTime);
+                if(presentHours<=4){
+                    halfDays ++;
+                }else{
+                    fullDays ++;
+                }
+                wage = EmpOpsHandler.wageCalculator(presentHours);
+                monthlyWage += wage;
+            }
         }
-        Employee emp = new Employee("Bhavesh", 1, setAttendance,wage);
-        if (emp.isPresentToday) {
-            System.out.println("Employee "+emp.name+" is present today");
-            System.out.println("Employees today wage is "+emp.wageDaily+" Rs.");
-        }else{
-            System.out.println("Employee "+emp.name+" is absent today");
-            emp.wageDaily = 0;
-            System.out.println("Employees today wage is "+emp.wageDaily+" Rs.");
-        }
+        System.out.println("Employee "+emp.name+" was present for a total of "+fullDays+" fulldays and "+halfDays+" halfdays based on that his total salary for month is "+monthlyWage+" Rs.");
     }
 }
