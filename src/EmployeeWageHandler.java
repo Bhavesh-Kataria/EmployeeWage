61d722324383f568e76f14e7240287ca86256b31
Bhavesh-Kataria <katariabhavesh6102000@gmail.com>
2025-03-13 11:51:47 +0530

    Added Employee's daily wages

--- src/EmployeeWageHandler.java
+++ src/EmployeeWageHandler.java
@@ -3,15 +3,17 @@ class Employee{
     String name;
     int id;
     boolean isPresentToday;
-    Employee(String name,int id,boolean isPresentToday){
+    int wageDaily;
+    Employee(String name,int id,boolean isPresentToday,int wageDaily){
         this.name = name;
         this.id = id;
         this.isPresentToday = isPresentToday;
+        this.wageDaily = wageDaily;
     }
 
     @Override
     public String toString() {
-        return "Employee [ "+"Name : "+this.name+", Id : "+this.id+", isPresent : "+this.isPresentToday+"]";
+        return "Employee [ "+"Name : "+this.name+", Id : "+this.id+", isPresent : "+this.isPresentToday+", Wage : "+this.wageDaily +"]";
     }
 }
 
@@ -20,17 +22,26 @@ class EmpOpsHandler{
         int isPresent = (int)Math.round(Math.random());
         return isPresent == 1 ;
     }
+    static int wageCalculator(){
+        final int hours = 8;
+        final int wagePerHour = 20;
+        return  hours * wagePerHour;
+    }
 }
 
 
 public class EmployeeWageHandler {
     public static void main(String[] args) {
         boolean setAttendance = EmpOpsHandler.empAttendance();
-        Employee emp = new Employee("Bhavesh", 1, setAttendance);
+        int wage = EmpOpsHandler.wageCalculator();
+        Employee emp = new Employee("Bhavesh", 1, setAttendance,wage);
         if (emp.isPresentToday) {
             System.out.println("Employee "+emp.name+" is present today");
+            System.out.println("Employees today wage is "+emp.wageDaily+" Rs.");
         }else{
             System.out.println("Employee "+emp.name+" is absent today");
+            emp.wageDaily = 0;
+            System.out.println("Employees today wage is "+emp.wageDaily+" Rs.");
         }
     }
 }
