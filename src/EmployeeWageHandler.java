d256a00817f4efc76a4828f5d6c1d40f6cc705f2
Bhavesh-Kataria <katariabhavesh6102000@gmail.com>
2025-03-17 11:29:53 +0530

    Added company's expense calculation

--- src/EmployeeWageHandler.java
+++ src/EmployeeWageHandler.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 class Employee{
+    Company c;
     String name;
     int id;
-    Employee(String name,int id){
+    Employee(String name,int id,Company c){
         this.name = name;
         this.id = id;
+        this.c = c;
+    }
+}
+
+class Company{
+    String name;
+    int minWorkingDays;
+    int minWorkingHours;
+    int monthlyWage;
+
+    public Company(String name, int minWorkingDays, int minWorkingHours, int monthlyWage) {
+        this.name = name;
+        this.minWorkingDays = minWorkingDays;
+        this.minWorkingHours = minWorkingHours;
+        this.monthlyWage = monthlyWage;
     }
 
-    @Override
-    public String toString() {
-        return "Employee [ "+"Name : "+this.name+", Id : "+this.id+"]";
-    }
 }
 
 class EmpOpsHandler{
@@ -70,24 +82,40 @@ class EmpOpsHandler{
         empData[2] = monthlyWage;
         return  empData;
     }
+    static void calculateCompanyExpense(Employee[] emps){
+         int[] companyExpense = new int[3];
+         for (Employee e : emps){
+             if(e!=null){
+                 switch (e.c.name) {
+                     case "QK" -> companyExpense[0] += e.c.monthlyWage;
+                     case "Wipro" -> companyExpense[1] += e.c.monthlyWage;
+                     case "Reliance" -> companyExpense[2] += e.c.monthlyWage;
+                 }
+             }
+         }
+        System.out.println("Monthly expense for Qk is Rs. "+companyExpense[0]);
+        System.out.println("Monthly expense for Wipro is Rs. "+companyExpense[1]);
+        System.out.println("Monthly expense for Reliance is Rs. "+companyExpense[2]);
+    }
 }
 
 
 public class EmployeeWageHandler {
     public static void main(String[] args) {
-        Employee emp = new Employee("Bhavesh", 1);
-        System.out.println("Enter no of full days and half days you have been present till date");
-        Scanner sc = new Scanner(System.in);
-        int fullDays = sc.nextInt();
-        int halfDays = sc.nextInt();
-        float totalDaysPresent = fullDays + ((float) halfDays /2);
-        int totalHoursPresent = fullDays * 8 + halfDays * 4;
-        if(totalDaysPresent < 20 && totalHoursPresent < 100){
-            System.out.println("Salary isn't generated yet");
-        } else if (totalDaysPresent < 20 && totalHoursPresent >= 100) {
-            System.out.println("Salary for "+totalHoursPresent+" hours is Rs. "+totalHoursPresent*20);
-        }else{
-            System.out.println("Salary for more than  20 days defaults to Rs. "+20*8*20);
-        }
+        Company c1= new Company("QK",14,100,3200);
+        Company c2= new Company("Wipro",12,98,4000);
+        Company c3= new Company("Reliance",18,150,2000);
+        Employee emp1 = new Employee("Bhavesh", 1,c1);
+        Employee emp2 = new Employee("Sahil", 2,c1);
+        Employee emp3 = new Employee("Rushikesh", 3,c3);
+        Employee emp4 = new Employee("Rehan", 4,c2);
+        Employee emp5 = new Employee("Dhruv", 5,c2);
+        Employee[] emps  = new Employee[10];
+        emps[0] = emp1;
+        emps[1] = emp2;
+        emps[2] = emp3;
+        emps[3] = emp4;
+        emps[4] = emp5;
+        EmpOpsHandler.calculateCompanyExpense(emps);
     }
 }
